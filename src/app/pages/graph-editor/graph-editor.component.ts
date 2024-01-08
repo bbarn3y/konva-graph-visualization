f87f8afa1b3eb78a8cf55e5fcdc965dffaeaf1e9
danielkapocsi <kapocsi.dani1@gmail.com>
2024-01-08 17:42:54 +0100

    #1 Group elements
    Bugfixed ungroup
    Updated delete

--- src/app/pages/graph-editor/graph-editor.component.ts
+++ src/app/pages/graph-editor/graph-editor.component.ts
@@ -104,7 +104,8 @@ export class GraphEditorComponent implements AfterViewInit {
       const shape = event.target;
       //console.log('groupID ' + shape.attrs.groupId);
       //console.log(shape, shape.getType());
-            
+      console.log("group lenth", this.groups.length);
+      
       //console.log('sr ', this.selectRectangle);
       
       if (event.target === this.stage){
@@ -290,131 +291,6 @@ export class GraphEditorComponent implements AfterViewInit {
       this.contextMenuElement.nativeElement.style.top = e.evt.clientY + 'px';
       this.contextMenuElement.nativeElement.style.left = e.evt.clientX + 'px';
     })
-
-    /*
-    this.stage.on('contextmenu', (e) => {
-      // prevent default behavior
-      e.evt.preventDefault();
-  
-      // Check if we are on an empty place of the stage
-      if (e.target === this.stage || this.selectedShape) {
-        return;
-      }
-
-      //if (!this.contextMenuElement) return;
-
-      //        if(!selectedShapes || selectedShapes.length <= 1) return;
-
-  
-      const currentShape = e.target;
-
-      //Check if target is grouped or not
-      if(e.target.attrs.group !== undefined) {
-        this.isGrouped = true;
-      } else {
-        this.isGrouped = false;
-      }
-  
-      // Get the context menu element
-      const contextMenu = document.getElementById('contextMenu');
-
-      if (contextMenu) {
-        // Set position based on the mouse pointer
-        contextMenu.style.display = 'block';
-        contextMenu.style.top = e.evt.clientY + 'px';
-        contextMenu.style.left = e.evt.clientX + 'px';
-
-        
-        // Handle context menu actions
-        document.getElementById('group_button')?.addEventListener('click', () => {
-          
-          const group = new Konva.Group();
-          console.log("new group made");
-          
-          group.attrs.id = this.getUniqueId(4);
-
-          const selectedShapes = this.stage?.find('Shape').filter(x => x.attrs.isSelected);
-        
-          if(!selectedShapes) return;
-          
-          // preprocess shapes if already in group(s)
-
-          const isRegroupingNeeded = selectedShapes.some(x => x.attrs.group !== undefined)
-          
-          console.log(isRegroupingNeeded);
-          
-
-          const groupsToDelete: Konva.Group[] = [];
-
-          selectedShapes.forEach(actShape => {
-            if (actShape.attrs.group !== undefined && !groupsToDelete.includes(actShape.attrs.group)){
-              groupsToDelete.push(actShape.attrs.group)
-              console.log('need to delete ', actShape.attrs.group.attrs.id);
-              
-              actShape.attrs.group = undefined;
-            }
-          });
-
-          
-          groupsToDelete.forEach(actGroup => {
-            const index = this.groups.indexOf(actGroup);
-
-            //console.log('deleted ', actGroup.attrs.groupId, ' at index', index);
-            
-
-            if (index !== -1) {
-              this.groups.splice(index, 1);
-            }
-          });
-          
-    
-          selectedShapes.forEach(actShape => {
-          
-            if (actShape instanceof Konva.Shape){
-              actShape.stroke('black');
-              actShape.attrs.groupId = group.attrs.id;
-              actShape.attrs.group = group;
-              group.add(actShape);
-            }
-            actShape.attrs.isSelected = false;
-          });
-
-          currentShape.attrs.group = group;
-          console.log(group.attrs.id);
-          
-          this.selectedLayer?.add(group);
-          this.selectedLayer?.draw();
-          this.groups.push(group);
-          contextMenu.style.display = 'none';
-        });
-        
-
-        document.getElementById('ungroup_button')?.addEventListener('click', () => {
-          const group = currentShape.attrs.group;
-          const sameGroupShapes = this.stage?.find('Shape').filter(x => x.attrs.group == group);
-          
-          sameGroupShapes.forEach(actShape => {
-            if (actShape instanceof Konva.Shape){
-              group.remove(actShape);
-              actShape.attrs.group = undefined;
-              actShape.stroke('black');
-            }
-          });
-          group.remove(currentShape);
-          currentShape.attrs.group = undefined;
-          contextMenu.style.display = 'none';
-        });
-
-        document.getElementById('delete_button')?.addEventListener('click', () => {
-          currentShape.destroy();
-          contextMenu.style.display = 'none';
-        });
-
-        // Add other context menu options and their respective handlers as needed
-      }
-    });
-    */
-    
 
     this.stage.on('mousedown', (event) => {
       // Store the starting point when the left mouse button is pressed
@@ -883,49 +759,14 @@ export class GraphEditorComponent implements AfterViewInit {
       }
       actShape.attrs.isSelected = false;
     });
-    
-    /*
-    selectedShapes.forEach(actShape => {
-      if (actShape.attrs.group !== undefined && !groupsToDelete.includes(actShape.attrs.group)){
-        groupsToDelete.push(actShape.attrs.group)
-        console.log('need to delete ', actShape.attrs.group.attrs.id);
-        
-        actShape.attrs.group = undefined;
-      }
-    });
+
+    //this.groups.filter(group => group !== groupToDelete);    
+    const index = this.groups.indexOf(groupToDelete);
+    if (index !== -1) {
+      this.groups.splice(index, 1);
+    }
 
     
-    groupsToDelete.forEach(actGroup => {
-      const index = this.groups.indexOf(actGroup);
-
-      //console.log('deleted ', actGroup.attrs.groupId, ' at index', index);
-      
-
-      if (index !== -1) {
-        this.groups.splice(index, 1);
-      }
-    });
-    
-    /*
-    selectedShapes.forEach(actShape => {
-    
-      if (actShape instanceof Konva.Shape){
-        actShape.stroke('black');
-        actShape.attrs.groupId = group.attrs.id;
-        actShape.attrs.group = group;
-        group.add(actShape);
-      }
-      actShape.attrs.isSelected = false;
-    });
-
-    //currentShape.attrs.group = group;
-    console.log(group.attrs.id);
-    
-    this.selectedLayer?.add(group);
-    this.selectedLayer?.draw();
-    this.groups.push(group);
-    */
-
     this.isShowContextMenu = false;
   }
 
@@ -936,38 +777,31 @@ export class GraphEditorComponent implements AfterViewInit {
     const sameGroupShapes = this.stage?.find('Shape').filter(x => x.attrs.group === this.clickedShape?.attrs.group);
 
     if (sameGroupShapes.length < 3){
-      const GroupToDelete = this.clickedShape.attrs.group;
+      const groupToDelete = this.clickedShape.attrs.group;
       
       sameGroupShapes.forEach(actShape => {
         actShape.attrs.group = undefined;
       });
 
-      //this.groups.remove
+      //this.groups.filter(group => group !== groupToDelete);
+      const index = this.groups.indexOf(groupToDelete);
+      if (index !== -1) {
+        this.groups.splice(index, 1);
+      }
     }
-
 
     this.clickedShape.destroy();
+    this.isShowContextMenu = false;
+
+    const selectedShapes = this.stage?.find('Shape').filter(x => x.attrs.isSelected);
+  
+    if(!selectedShapes) return;
+
+    selectedShapes.forEach(actShape => {
+      if (actShape instanceof Konva.Shape){
+        actShape.stroke('black');
+      }
+      actShape.attrs.isSelected = false;
+    });
   }
-
-/*
-  showContextMenu(event: MouseEvent): void {
-    // Get the context menu element
-    const contextMenu = document.getElementById('contextMenu');
-
-    if (contextMenu) {
-      // Set position based on the mouse pointer
-      contextMenu.style.display = 'block';
-      contextMenu.style.top = event.clientY + 'px';
-      contextMenu.style.left = event.clientX + 'px';
-
-      // Handle context menu actions
-      document.getElementById('delete_button')?.addEventListener('click', () => {
-        this.currentShape.destroy();
-        contextMenu.style.display = 'none';
-      });
-
-      // Add other context menu options and their respective handlers as needed
-    }
-  }
-  */
 }
